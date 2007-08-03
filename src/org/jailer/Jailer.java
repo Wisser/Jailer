e726cc6ac9605c0b668db2d2e4352a7918ed631e
R Wisser <rwisser@users.sourceforge.net>
2007-08-03 08:11:43 +0000

    set VERION to 1.0.1

--- src/org/jailer/Jailer.java
+++ src/org/jailer/Jailer.java
@@ -70,7 +70,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     
     /**
      * The relational data model.
