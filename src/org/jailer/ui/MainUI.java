cfbad3c7cbdc001a44ae915fe319634657c97512
R Wisser <rwisser@users.sourceforge.net>
2007-11-14 08:58:16 +0000

    model editor

--- src/org/jailer/ui/MainUI.java
+++ src/org/jailer/ui/MainUI.java
@@ -21,6 +21,19 @@ import javax.swing.JTabbedPane;
 
 import org.jailer.Jailer;
 
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
 public class MainUI extends javax.swing.JFrame {
 
     private JTabbedPane jTabbedPane = null;
@@ -63,6 +76,7 @@ public class MainUI extends javax.swing.JFrame {
             jTabbedPane.addTab("Print Model", null, getPrintDatamodelUI(), null);
             jTabbedPane.addTab("Create DDL", null, getCreateDDLUI(), null);
             jTabbedPane.addTab("Export", null, getExportUI(), null);
+            jTabbedPane.addTab("Model Editor", null, new ModelEditor(), null);
         }
         return jTabbedPane;
     }
