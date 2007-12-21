85fc4c645232a8b0b1ff27b02a33218cb12c8517
R Wisser <rwisser@users.sourceforge.net>
2007-12-21 13:49:54 +0000

    2.1.2

--- src/net/sf/jailer/Jailer.java
+++ src/net/sf/jailer/Jailer.java
@@ -72,7 +72,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "2.1.1";
+    public static final String VERSION = "2.1.2";
     
     /**
      * The relational data model.
