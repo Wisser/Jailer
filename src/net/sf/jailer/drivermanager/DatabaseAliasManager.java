6a9b70dc2394fdf0f4c73ac8083242c23e2dcd32
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2008-02-11 13:49:22 +0000

    created
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@161 3dd849cd-670e-4645-a7cd-dd197c8d0e81

---
+++ src/net/sf/jailer/drivermanager/DatabaseAliasManager.java
@@ -0,0 +1,58 @@
+package net.sf.jailer.drivermanager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Manages {@link DatabaseAlias}es
+ * <ul>
+ *   <li>Persistency
+ *   <li>Establishment of database connections
+ * </ul>
+ */
+public final class DatabaseAliasManager {
+	
+	/**
+	 * Registers a {@link DatabaseAlias} under a given name.
+	 * Removes any alias previously registered under same name.
+	 * 
+	 * @param name the alias name
+	 * @param alias the alias to register
+	 */
+	public static void registerAlias(String name, DatabaseAlias alias) {
+		...
+	}
+
+	/**
+	 * Gets a registered {@link DatabaseAlias}.
+	 * 
+	 * @param name the name under which the alias has been registered
+	 * @return the alias or null, if no alias has been registered under given name
+	 */
+	public static DatabaseAlias getAlias(String name) {
+		...
+	}
+
+	/**
+	 * Removes a {@link DatabaseAlias}.
+	 * 
+	 * @param name the name under which the alias to be removed has been registered
+	 */
+	public static void removeAlias(String name) {
+		...
+	}
+
+	/**
+	 * Connect to a database schema.
+	 * 
+	 * @param alias alias of database schema
+	 * @param password password for authentification
+	 * @return a connection to the database
+	 * 
+	 * @throws SQLException if connection cannot be established
+	 */
+	public static Connection connect(DatabaseAlias alias, String password) throws SQLException {
+		...
+	}
+	
+}
