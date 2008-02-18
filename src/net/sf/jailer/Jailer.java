fbcc0b1252ae06406b1b242963422d34315f2873
R Wisser <rwisser@users.sourceforge.net>
2008-02-18 08:27:41 +0000

    made compile clean

--- src/net/sf/jailer/Jailer.java
+++ src/net/sf/jailer/Jailer.java
@@ -60,7 +60,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "2.1.4";
+    public static final String VERSION = "2.1.5";
     
     /**
      * The relational data model.
