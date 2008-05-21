3b63b6d1562bc08362ab62c98f6e434bdfd3f7e5
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2008-05-21 20:54:46 +0000

    fixed a bug that occurred when more than one connection to dbms is used simultaniously during exportation due to lack of thread safeness of jav.text.DateFormat
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@360 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/net/sf/jailer/Jailer.java
+++ src/net/sf/jailer/Jailer.java
@@ -73,7 +73,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "2.2.0";
+    public static final String VERSION = "2.2.1";
     
     /**
      * The relational data model.
