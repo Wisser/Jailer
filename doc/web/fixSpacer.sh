d172ec984080ba54c97f71c93b4eeccbabb90590
R Wisser <rwisser@users.sourceforge.net>
2008-06-17 07:28:20 +0000

    XML tutorial

--- doc/web/fixSpacer.sh
+++ doc/web/fixSpacer.sh
@@ -1,1 +1,3 @@
 find . -iname "*.htm" -exec sed "s|class=\"spacer\" .*width=\"100%\"> *<br>|class=\"spacer\" width=\"100%\">|g" '{}' --in-place  \;
+find . -iname "*.html" -exec sed "s|class=\"spacer\" .*width=\"100%\"> *<br>|class=\"spacer\" width=\"100%\">|g" '{}' --in-place  \;
+
