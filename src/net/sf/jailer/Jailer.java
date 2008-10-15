608c736690b3d773d874450a4f235f37454f66a6
R Wisser <rwisser@users.sourceforge.net>
2008-10-15 15:40:59 +0000

    search timestamp pattern even if dummy values are already persisted

--- src/net/sf/jailer/Jailer.java
+++ src/net/sf/jailer/Jailer.java
@@ -78,7 +78,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "2.5.2";
+    public static final String VERSION = "2.5.3.rc1";
     
     /**
      * The relational data model.
