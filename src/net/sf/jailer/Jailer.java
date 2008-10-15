9f0d2a1a42e0a097964f4606abca297731a08bec
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2008-10-15 15:46:42 +0000

    search timestamp pattern even if dummy values are already persisted
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@453 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/net/sf/jailer/Jailer.java
+++ src/net/sf/jailer/Jailer.java
@@ -78,7 +78,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "2.5.3.rc1";
+    public static final String VERSION = "2.5.3";
     
     /**
      * The relational data model.
