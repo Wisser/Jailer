bd48a026303dfda057af7f1b4145cc022869db69
R Wisser <rwisser@users.sourceforge.net>
2008-10-28 07:43:00 +0000

    added release note for 2.5.7

--- src/net/sf/jailer/Jailer.java
+++ src/net/sf/jailer/Jailer.java
@@ -79,7 +79,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "2.5.7.beta";
+    public static final String VERSION = "2.5.7";
     
     /**
      * The relational data model.
