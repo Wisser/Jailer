3a533a9b00e8231ad59d390dcf10ee808e7320a9
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2008-11-12 07:48:03 +0000

    rendering of associations' cardinality
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@461 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/net/sf/jailer/Jailer.java
+++ src/net/sf/jailer/Jailer.java
@@ -79,7 +79,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "2.5.8.beta";
+    public static final String VERSION = "2.5.8";
     
     /**
      * The relational data model.
