e578f2990408129a6db4b78940eb104d49113654
R Wisser <rwisser@users.sourceforge.net>
2009-03-03 07:46:41 +0000

    added setting for Sybase jConnect 6.0

--- src/net/sf/jailer/Jailer.java
+++ src/net/sf/jailer/Jailer.java
@@ -86,7 +86,7 @@ public class Jailer {
     /**
      * The Jailer version.
      */
-    public static final String VERSION = "2.9.0.beta";
+    public static final String VERSION = "2.9.0";
     
     /**
      * The relational data model.
