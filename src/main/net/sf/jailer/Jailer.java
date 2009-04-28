44f82119a5a1735efea077cbd667105dae557a0e
R Wisser <rwisser@users.sourceforge.net>
2009-04-28 06:30:07 +0000

    added column replacements for PostgreSQL

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -91,7 +91,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "2.9.6";
+	public static final String VERSION = "2.9.7";
 
 	/**
 	 * The relational data model.
