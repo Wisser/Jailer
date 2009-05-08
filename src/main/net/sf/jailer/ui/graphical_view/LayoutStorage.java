28b2529ed1836c25f273a0cf64be0ec55bca1ab3
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2009-05-08 11:18:15 +0000

    Graph layout is persistent
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@616 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/ui/graphical_view/LayoutStorage.java
+++ src/main/net/sf/jailer/ui/graphical_view/LayoutStorage.java
@@ -16,7 +16,6 @@
 package net.sf.jailer.ui.graphical_view;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.HashMap;
@@ -32,7 +31,7 @@ import net.sf.jailer.util.CsvFile;
 public class LayoutStorage {
     
 	/**
-	 * File holding global layouts.
+	 * Folder holding global layouts.
 	 */
 	private static final String GLOBAL_STORAGE = ".layout";
 	
@@ -59,6 +58,75 @@ public class LayoutStorage {
 	}
 
 	/**
+	 * Removes layout if it is not significant.
+	 * 
+	 * @param rootTable root table for which the layout is
+	 */
+	public static void checkSignificance(String rootTable) {
+		Map<String, double[]> layout = tablePositions.get(rootTable);
+		if (!isSignificant(rootTable, layout)) {
+			tablePositions.remove(rootTable);
+		} else {
+			globalTablePositions.put(rootTable, new HashMap<String, double[]>(layout));
+		}
+	}
+
+	/**
+	 * Checks whether a layout is significant.
+	 * 
+	 * @param rootTable root table for which the layout is
+	 * @param layout the layout
+	 * @return <code>true</code> if layout is significant
+	 */
+	private static boolean isSignificant(String rootTable, Map<String, double[]> layout) {
+		if (layout != null) {
+			for (String t: layout.keySet()) {
+				if (!t.equals(rootTable) && !"$ZOOMBOX".equals(t)) {
+					if (layout.get(t)[2] == 1.0) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Gets file to store global layout in for a given root table.
+	 * 
+	 * @param rootTable the root table
+	 * @return file to store global layout in, <code>null</code> if the file cannot be created
+	 */
+	private static File getGlobalStorageFile(String rootTable) {
+		try {
+			File dir = new File(GLOBAL_STORAGE);
+			if (!dir.exists()) {
+				if (!dir.mkdir()) {
+					return null;
+				}
+			}
+			String fn = "";
+			for (int i = 0; i < rootTable.length(); ++i) {
+				char c = rootTable.charAt(i);
+				if (Character.isLetterOrDigit(c) || c == '.') {
+					fn += c;
+				}
+			}
+			if (fn.length() > 0) {
+				return new File(dir, fn);
+			}
+			return null;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	/**
+	 * If storage is disabled, no persistent layout is available.
+	 */
+	public volatile static boolean enabled = true;
+	
+	/**
 	 * Gets position.
 	 * 
 	 * @param rootTable root table
@@ -66,9 +134,12 @@ public class LayoutStorage {
 	 * @return position: x, y, isFixed
 	 */
 	public static double[] getPosition(String rootTable, String table) {
-		Map<String, double[]> posMap = tablePositions.get(rootTable);
-		if (posMap != null) {
-			return posMap.get(table);
+		if (enabled) {
+			readGlobalLayout(rootTable);
+			Map<String, double[]> posMap = tablePositions.get(rootTable);
+			if (posMap != null) {
+				return posMap.get(table);
+			}
 		}
 		return null;
 	}
@@ -80,7 +151,11 @@ public class LayoutStorage {
 	 * @return positions of all tables for a the root-table
 	 */
 	public static Map<String, double[]> getPositions(String root) {
-		return tablePositions.get(root);
+		if (enabled) {
+			readGlobalLayout(root);
+			return tablePositions.get(root);
+		}
+		return null;
 	}
 
 	/**
@@ -93,19 +168,13 @@ public class LayoutStorage {
 	}
 
 	/**
-	 * Store layout into stream and {@link #GLOBAL_STORAGE}.
+	 * Removes all position information.
 	 * 
-	 * @param out the stream
+	 * @param root the root table
 	 */
-	public static void store(PrintWriter out) {
-		store0(out);
-//		try {
-//			PrintWriter gout = new PrintWriter(new File(GLOBAL_STORAGE));
-//			store0(gout);
-//			gout.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
+	public static void removeAll() {
+		tablePositions.clear();
+		globalTablePositions.clear();
 	}
 
 	/**
@@ -113,10 +182,68 @@ public class LayoutStorage {
 	 * 
 	 * @param out the stream
 	 */
-	private static void store0(PrintWriter out) {
+	public static void store(PrintWriter out) {
+		store(out, tablePositions);
+		
+		try {
+			for (String rootTable: tablePositions.keySet()) {
+				Map<String, double[]> layout = globalTablePositions.get(rootTable);
+				if (layout == null) {
+					continue;
+				}
+				globalTablePositions.put(rootTable, new HashMap<String, double[]>(layout));
+				Map<String, Map<String, double[]>> tmp = new HashMap<String, Map<String,double[]>>();
+				tmp.put(rootTable, layout);
+				File file = getGlobalStorageFile(rootTable);
+				if (file != null) {
+					PrintWriter tout = new PrintWriter(file);
+					store(tout, tmp);
+					tout.close();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Reads global layout if local layout does not exist.
+	 * 
+	 * @param rootTable
+	 */
+	private static void readGlobalLayout(String rootTable) {
+		if (!tablePositions.containsKey(rootTable)) {
+			if (!globalTablePositions.containsKey(rootTable)) {
+				File file = getGlobalStorageFile(rootTable);
+				Map<String, double[]> layout = null;
+				if (file != null) {
+					try {
+						restore(file.getAbsolutePath(), false);
+						if (tablePositions.get(rootTable) != null) {
+							layout = new HashMap<String, double[]>(tablePositions.get(rootTable));
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				globalTablePositions.put(rootTable, layout);
+			}
+			Map<String, double[]> layout = globalTablePositions.get(rootTable);
+			if (layout != null) {
+				tablePositions.put(rootTable, new HashMap<String, double[]>(globalTablePositions.get(rootTable)));
+			}
+		}
+	}
+
+	/**
+	 * Store layout into stream.
+	 * 
+	 * @param out the stream
+	 */
+	private static void store(PrintWriter out, Map<String, Map<String, double[]>> positions) {
 		out.println(CsvFile.BLOCK_INDICATOR + "layout");
-		for (String root: tablePositions.keySet()) {
-			Map<String, double[]> pos = tablePositions.get(root);
+		for (String root: positions.keySet()) {
+			Map<String, double[]> pos = positions.get(root);
 			for (String table: pos.keySet()) {
 				double[] xyf = pos.get(table);
 				out.println(CsvFile.encodeCell(root) + "; " + CsvFile.encodeCell(table) + "; " + xyf[0] + "; " + xyf[1] + "; " + xyf[2]);
@@ -130,21 +257,36 @@ public class LayoutStorage {
 	 * @param file the file
 	 */
 	public static void restore(String file) {
-		tablePositions.clear();
+		restore(file, true);
+	}
+
+	/**
+	 * Restores a layout from file
+	 *  
+	 * @param file the file
+	 */
+	private static void restore(String file, boolean resetLayout) {
+		if (resetLayout) {
+			tablePositions.clear();
+			globalTablePositions.clear();
+		}
 		try {
-			CsvFile csv = new CsvFile(new File(file), "layout");
-			for (CsvFile.Line line: csv.getLines()) {
-				if (line.cells.get(0).length() > 0) {
-					double x = 0.0;
-					double y = 0.0;
-					double f = 0.0;
-					try {
-						x = Double.parseDouble(line.cells.get(2));
-						y = Double.parseDouble(line.cells.get(3));
-						f = Double.parseDouble(line.cells.get(4));
-						setPosition(line.cells.get(0), line.cells.get(1), new double[] { x, y, f });
-					} catch (Exception e) {
-						//ignore
+			File csvFile = new File(file);
+			if (csvFile.exists()) {
+				CsvFile csv = new CsvFile(csvFile, "layout");
+				for (CsvFile.Line line: csv.getLines()) {
+					if (line.cells.get(0).length() > 0) {
+						double x = 0.0;
+						double y = 0.0;
+						double f = 0.0;
+						try {
+							x = Double.parseDouble(line.cells.get(2));
+							y = Double.parseDouble(line.cells.get(3));
+							f = Double.parseDouble(line.cells.get(4));
+							setPosition(line.cells.get(0), line.cells.get(1), new double[] { x, y, f });
+						} catch (Exception e) {
+							//ignore
+						}
 					}
 				}
 			}
@@ -154,8 +296,13 @@ public class LayoutStorage {
 	}
 
 	/**
-	 * Stores positions per root-table.
+	 * Stores positions per root-table (local).
 	 */
 	private static Map<String, Map<String, double[]>> tablePositions = Collections.synchronizedMap(new HashMap<String, Map<String,double[]>>());
+
+	/**
+	 * Stores positions per root-table (global).
+	 */
+	private static Map<String, Map<String, double[]>> globalTablePositions = Collections.synchronizedMap(new HashMap<String, Map<String,double[]>>());
 
 }
