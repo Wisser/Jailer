2c31666171895f64ce6a1cdab1bf9fc3962b01d2
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2009-10-02 13:33:18 +0000

    Fix for bug 2871798 concerning importation of multi line CLOBs.
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@690 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -96,7 +96,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "3.1.7";
+	public static final String VERSION = "3.1.8.beta";
 
 	/**
 	 * The relational data model.
