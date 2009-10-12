36d9966a108424be7143e1da31b5ba23965cb7f9
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2009-10-12 09:42:40 +0000

    Set maximum table name length to 120 characters
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@693 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -96,7 +96,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "3.1.8";
+	public static final String VERSION = "3.1.9";
 
 	/**
 	 * The relational data model.
