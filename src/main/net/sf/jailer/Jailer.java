d4659093c4a5c008e6bdbb55361be38315506dd0
R Wisser <rwisser@users.sourceforge.net>
2010-02-17 11:39:59 +0000

    FlatXML export with NULL columns placeholder [2953453]

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -96,7 +96,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "3.3.beta";
+	public static final String VERSION = "3.3";
 
 	/**
 	 * The relational data model.
