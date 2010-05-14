88e0ed80c084246ad75ae6dff9797b5c67d4f146
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2010-05-14 07:03:13 +0000

    added support for user-defined distinct types
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@718 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -98,7 +98,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "3.4.2";
+	public static final String VERSION = "3.4.3";
 
 	/**
 	 * The relational data model.
