2eb03cb63b12faea7f2d3ccac4942628c65485be
R Wisser <rwisser@users.sourceforge.net>
2010-10-27 13:22:35 +0000

    close connection after DDL creation

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -100,7 +100,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "3.4.7";
+	public static final String VERSION = "3.4.8";
 
 	/**
 	 * The relational data model.
