ec75358c85fab409a0c907d024a69dc90d1d53e3
R Wisser <rwisser@users.sourceforge.net>
2010-12-10 08:05:15 +0000

    preparing release 3.4.9

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -100,7 +100,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "3.4.8";
+	public static final String VERSION = "3.4.9";
 
 	/**
 	 * The relational data model.
