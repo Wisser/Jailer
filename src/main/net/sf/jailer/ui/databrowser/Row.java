3c8b71047b08894629dfbaa6464595ed764652e5
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2010-12-27 15:41:28 +0000

    new
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@738 3dd849cd-670e-4645-a7cd-dd197c8d0e81

---
+++ src/main/net/sf/jailer/ui/databrowser/Row.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sf.jailer.ui.databrowser;
+
+/**
+ * A row of a database table to be rendered in a row-browser.
+ * 
+ * @author Ralf Wisser
+ */
+public class Row {
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param rowId unique ID, also serves as SQL predicate which identifies this row
+	 * @param values column values
+	 */
+	public Row(String rowId, String[] values) {
+		this.rowId = rowId;
+		this.values = values;
+	}
+	
+	/**
+	 * Unique ID, also serves as SQL predicate which identifies this row.
+	 */
+	public final String rowId;
+	
+	/**
+	 * Column values.
+	 */
+	public final String[] values;
+	
+}
