8edde6cd676b3a5f1ae9af18388d0e2d039cd1bf
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2011-01-17 14:54:07 +0000

    preparing release 3.5.1
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@754 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -100,7 +100,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "3.5";
+	public static final String VERSION = "3.5.1";
 
 	/**
 	 * The relational data model.
