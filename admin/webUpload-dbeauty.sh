2ba3a8878fb92230c5ef337c8ba870b63168b390
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2011-09-14 09:34:32 +0000

    fixed forum URL
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@786 3dd849cd-670e-4645-a7cd-dd197c8d0e81

---
+++ admin/webUpload-dbeauty.sh
@@ -0,0 +1,24 @@
+rm -rf ~/tmp/jailer
+rm -rf ~/tmp/1
+rm -rf ~/tmp/1.co
+mkdir ~/tmp/jailer
+mkdir ~/tmp/1
+mkdir ~/tmp/1.co
+cd ~/tmp/1.co
+svn co https://jailer.svn.sf.net/svnroot/jailer/trunk/doc/web/dbeauty
+cd ..
+mv 1.co/dbeauty/* jailer
+cd jailer
+
+find . -iname "*.htm" -exec sed "s|class=\"spacer\" .*width=\"100%\"> *<br>|class=\"spacer\" width=\"100%\">|g" '{}' --in-place  \;
+find . -iname "*.html" -exec sed "s|class=\"spacer\" .*width=\"100%\"> *<br>|class=\"spacer\" width=\"100%\">|g" '{}' --in-place  \;
+
+sed "s|</body>|<script type=\"text/javascript\">var pkBaseURL = ((\"https:\" == document.location.protocol) ? \"https://sourceforge.net/apps/piwik/jailer/\" : \"http://sourceforge.net/apps/piwik/jailer/\"); document.write(unescape(\"%3Cscript src='\" + pkBaseURL + \"piwik.js' type='text/javascript'%3E%3C/script%3E\")); </script><script type=\"text/javascript\"> try { var piwikTracker = Piwik.getTracker(pkBaseURL + \"piwik.php\", 2); piwikTracker.trackPageView(); piwikTracker.enableLinkTracking(); } catch( err ) {} </script></body>|g" *.htm --in-place
+sed "s|</body>|<script type=\"text/javascript\">var pkBaseURL = ((\"https:\" == document.location.protocol) ? \"https://sourceforge.net/apps/piwik/jailer/\" : \"http://sourceforge.net/apps/piwik/jailer/\"); document.write(unescape(\"%3Cscript src='\" + pkBaseURL + \"piwik.js' type='text/javascript'%3E%3C/script%3E\")); </script><script type=\"text/javascript\"> try { var piwikTracker = Piwik.getTracker(pkBaseURL + \"piwik.php\", 2); piwikTracker.trackPageView(); piwikTracker.enableLinkTracking(); } catch( err ) {} </script></body>|g" *.html --in-place
+
+cp home-db.htm index.html
+find -iname ".svn" -exec rm -rf '{}' \;
+find -iname ".cvs*" -exec rm -rf '{}' \;
+
+scp -r * rwisser,dbeauty@web.sf.net:/home/project-web/dbeauty/htdocs/
+
