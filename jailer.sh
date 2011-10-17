f67e93b5313360618b5240d72beca1a1a0cee096
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2011-10-17 17:31:55 +0000

    embedded H2SQL as demo DBMS
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@827 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- jailer.sh
+++ jailer.sh
@@ -14,7 +14,6 @@ CP=$CP:$LIB/log4j.jar
 CP=$CP:$LIB/args4j.jar
 CP=$CP:$LIB/spring.jar
 CP=$CP:$LIB/sdoc-0.5.0-beta.jar
-CP=$CP:$LIB/h2-1.3.160.jar
 CP=$CP:jailer.jar
 
 java -Xmx1024M -cp $CP net.sf.jailer.Jailer "$@"
