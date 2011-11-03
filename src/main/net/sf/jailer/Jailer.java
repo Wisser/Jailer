33d8b17a117976871cb5db59e36422f1c9feaac1
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2011-11-03 10:39:43 +0000

    fix for bug 3432301
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@860 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -100,7 +100,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "3.7.5";
+	public static final String VERSION = "3.7.6";
 	
 	/**
 	 * The Jailer application name.
