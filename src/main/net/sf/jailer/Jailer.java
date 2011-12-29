89d6ed6b41af5b92d9c02749b0c2328510735445
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2011-12-29 10:19:20 +0000

    Support for DB2's FIELD PROCS (experimental)
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@928 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -100,7 +100,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "4.0.3";
+	public static final String VERSION = "4.0.4.beta";
 	
 	/**
 	 * The Jailer application name.
