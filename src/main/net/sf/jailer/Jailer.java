34ba4227b32c7a10ae395e48a0e166acf16080e8
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2012-02-28 13:23:41 +0000

    always reload child-browsers
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@936 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -100,7 +100,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "4.0.4";
+	public static final String VERSION = "4.0.5";
 	
 	/**
 	 * The Jailer application name.
