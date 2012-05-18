c3952247a61d769864df90674a607880d35e9c3a
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2012-05-18 12:46:27 +0000

    release 4.0.8
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@940 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -100,7 +100,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "4.0.7";
+	public static final String VERSION = "4.0.8";
 	
 	/**
 	 * The Jailer application name.
