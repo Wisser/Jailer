4efc9ff1bb43e674afbaa15aba133ec21dcdc420
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2012-09-24 08:56:04 +0000

    Added support for high-resolution mouse-wheels
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@953 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -100,7 +100,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "4.0.14";
+	public static final String VERSION = "4.0.15";
 	
 	/**
 	 * The Jailer application name.
