377b236cd9a7fb3f980f3e9d8ce7b8482cf973cc
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2016-09-12 07:12:04 +0000

    6.0.1
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1081 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- script/ddl-template.sql
+++ script/ddl-template.sql
@@ -12,7 +12,7 @@ ${create-table}${schema}JAILER_CONFIG${table-suffix}
    jvalue          VARCHAR(200)
 ) ${create-table-suffix};
 
-INSERT INTO ${config-dml-reference}${table-suffix}(jversion, jkey, jvalue) values('${version}', 'magic', '837065098274756382534403654245288');
+INSERT INTO ${schema}${config-dml-reference}${table-suffix}(jversion, jkey, jvalue) values('${version}', 'magic', '837065098274756382534403654245288');
 
 ${create-table}${schema}JAILER_GRAPH${table-suffix}
 (
@@ -38,8 +38,8 @@ ${create-table}${schema}JAILER_ENTITY${table-suffix}
 -- ,  CONSTRAINT jlr_fk_graph_e FOREIGN KEY (r_entitygraph) REFERENCES ${schema}JAILER_GRAPH${table-suffix}(id)
 ) ${create-table-suffix};
 
-${create-index}${schema}jlr_enty_brthdy${table-suffix} ON ${index-table-prefix}${schema}JAILER_ENTITY${table-suffix} (r_entitygraph, birthday, type) ${create-index-suffix};
-${for-each:column-list}${create-index}${schema}jlr_enty_upk${table-suffix}$i ON ${index-table-prefix}${schema}JAILER_ENTITY${table-suffix} (r_entitygraph $, type) ${create-index-suffix};
+${create-index}${index-schema}jlr_enty_brthdy${table-suffix} ON ${index-table-prefix}${schema}JAILER_ENTITY${table-suffix} (r_entitygraph, birthday, type) ${create-index-suffix};
+${for-each:column-list}${create-index}${index-schema}jlr_enty_upk${table-suffix}$i ON ${index-table-prefix}${schema}JAILER_ENTITY${table-suffix} (r_entitygraph $, type) ${create-index-suffix};
 ${end}
 
 ${create-table}${schema}JAILER_SET${table-suffix}
@@ -49,7 +49,7 @@ ${create-table}${schema}JAILER_SET${table-suffix}
    ${upk}
 ) ${create-table-suffix};
 
-${for-each:column-list}${create-index}${schema}jlr_pk_set${table-suffix}$i ON ${index-table-prefix}${schema}JAILER_SET${table-suffix} (set_id $, type) ${create-index-suffix};
+${for-each:column-list}${create-index}${index-schema}jlr_pk_set${table-suffix}$i ON ${index-table-prefix}${schema}JAILER_SET${table-suffix} (set_id $, type) ${create-index-suffix};
 ${end}
 
 ${create-table}${schema}JAILER_DEPENDENCY${table-suffix}
@@ -66,9 +66,9 @@ ${create-table}${schema}JAILER_DEPENDENCY${table-suffix}
 -- ,  CONSTRAINT jlr_fk_graph_d FOREIGN KEY (r_entitygraph) REFERENCES ${schema}JAILER_GRAPH${table-suffix}(id)
 ) ${create-table-suffix};
 
-${for-each:column-list-from}${create-index}${schema}jlr_dep_from${table-suffix}$i ON ${index-table-prefix}${schema}JAILER_DEPENDENCY${table-suffix} (r_entitygraph, assoc $) ${create-index-suffix};
+${for-each:column-list-from}${create-index}${index-schema}jlr_dep_from${table-suffix}$i ON ${index-table-prefix}${schema}JAILER_DEPENDENCY${table-suffix} (r_entitygraph, assoc $) ${create-index-suffix};
 ${end}
-${for-each:column-list-to}${create-index}${schema}jlr_dep_to${table-suffix}$i ON ${index-table-prefix}${schema}JAILER_DEPENDENCY${table-suffix} (r_entitygraph $) ${create-index-suffix};
+${for-each:column-list-to}${create-index}${index-schema}jlr_dep_to${table-suffix}$i ON ${index-table-prefix}${schema}JAILER_DEPENDENCY${table-suffix} (r_entitygraph $) ${create-index-suffix};
 ${end}
 
 ${create-table}${schema}JAILER_TMP${table-suffix} 
@@ -77,4 +77,4 @@ ${create-table}${schema}JAILER_TMP${table-suffix}
     c2 INTEGER
 ) ${create-table-suffix};
 
-INSERT INTO ${config-dml-reference}${table-suffix}(jversion, jkey, jvalue) values('${version}', 'upk', '${upk-hash}');
+INSERT INTO ${schema}${config-dml-reference}${table-suffix}(jversion, jkey, jvalue) values('${version}', 'upk', '${upk-hash}');
