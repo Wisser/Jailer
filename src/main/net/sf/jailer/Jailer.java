fc59cb6517ae535ce56872ae17317adc56f2f2b6
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2016-09-17 15:16:23 +0000

    Fixed an incompatibility with Java 9
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1082 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -107,7 +107,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "6.0.1";
+	public static final String VERSION = "6.0.2";
 	
 	/**
 	 * The Jailer application name.
