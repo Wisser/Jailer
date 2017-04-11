461afd627ab62630f436dc5122b71bd18a9248de
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2017-04-11 11:07:46 +0000

    Minor bug fixes
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1194 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/net/sf/jailer/Jailer.java
+++ src/main/net/sf/jailer/Jailer.java
@@ -61,7 +61,7 @@ public class Jailer {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "6.7.2";
+	public static final String VERSION = "6.7.3";
 	
 	/**
 	 * The Jailer application name.
