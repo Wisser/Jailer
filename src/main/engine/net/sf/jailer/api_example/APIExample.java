6b1c461e50a329ef5044540b747c47bb18f06268
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2017-05-09 08:09:13 +0000

    renamed
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1252 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/engine/net/sf/jailer/api_example/APIExample.java
+++ src/main/engine/net/sf/jailer/api_example/APIExample.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.sf.jailer.example;
+package net.sf.jailer.api_example;
 
 import java.io.File;
 import java.io.IOException;
