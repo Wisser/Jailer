377dab12dceb26fbe6cd54c70654d79a8225ba45
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2017-06-02 06:12:14 +0000

    use user's home directory
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1295 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "7.0.3";
+	public static final String VERSION = "7.0.4";
 	
 	/**
 	 * The Jailer working tables version.
