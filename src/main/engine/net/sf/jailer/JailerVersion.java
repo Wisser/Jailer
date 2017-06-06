7be3b0b761992d4f73f0952a43247d72b70d2336
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2017-06-06 13:08:28 +0000

    Fixed a bug concerning temporary-table support
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1296 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "7.0.4";
+	public static final String VERSION = "7.0.5.b";
 	
 	/**
 	 * The Jailer working tables version.
