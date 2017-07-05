abe8644738f02e2c94aba7a0591ba1779f9465f4
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2017-07-05 10:45:42 +0000

    Small fixes for Java 9 compatibility
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1307 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- jailerDataBrowser.sh
+++ jailerDataBrowser.sh
@@ -19,6 +19,10 @@ CP=$CP:$LIB/args4j.jar
 CP=$CP:$LIB/spring.jar
 CP=$CP:$LIB/prefuse.jar
 CP=$CP:$LIB/sdoc-0.5.0-beta.jar
+CP=$CP:$LIB/activation-1.0.2.jar
+CP=$CP:$LIB/jaxb-core-2.3.0-b170127.1453.jar
+CP=$CP:$LIB/jaxb-impl-2.3.0-b170127.1453.jar
+CP=$CP:$LIB/jaxb-api-2.3.0-b170201.1204.jar
 CP=$CP:jailer.jar
 
 # echo $CP
--- jailerGUI.bat
+++ jailerGUI.bat
@@ -10,6 +10,10 @@ set CP=%CP%;%LIB%\log4j.jar
 set CP=%CP%;%LIB%\args4j.jar
 set CP=%CP%;%LIB%\prefuse.jar
 set CP=%CP%;%LIB%\sdoc-0.5.0-beta.jar
+set CP=%CP%;%LIB%\activation-1.0.2.jar
+set CP=%CP%;%LIB%\jaxb-core-2.3.0-b170127.1453.jar
+set CP=%CP%;%LIB%\jaxb-impl-2.3.0-b170127.1453.jar
+set CP=%CP%;%LIB%\jaxb-api-2.3.0-b170201.1204.jar
 set CP=%CP%;jailer.jar
 
 start javaw -Xmx1024M -cp %CP% net.sf.jailer.ui.ExtractionModelFrame %*
