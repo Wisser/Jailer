b4dcf9ed2ef0f3f00f27e5bcdda98b0582db517d
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2017-07-26 08:17:29 +0000

    Introduction of the "expansion limit" feature for improved navigation
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1312 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/gui/net/sf/jailer/ui/NeighborhoodPanel.java
+++ src/main/gui/net/sf/jailer/ui/NeighborhoodPanel.java
@@ -19,6 +19,12 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -30,6 +36,10 @@ import java.util.Set;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.SwingUtilities;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 
 import net.sf.jailer.datamodel.Association;
 import net.sf.jailer.datamodel.DataModel;
@@ -44,7 +54,19 @@ import net.sf.jailer.ui.graphical_view.GraphicalDataModelView;
 public class NeighborhoodPanel extends javax.swing.JPanel {
 
     private static final Color WHITE = new Color(255, 255, 255, 200);
+    private static final Color SELECTED_COLOR = new Color(0, 255, 255, 150);
+    
+    private int length = 2;
+    private int offset = 0;
+    
     private final Font italicFont;
+
+    private final Color depOnColor;
+    private final Color hasDepColor;
+    private final Color assocWithColor;
+    private final Color ignoredColor;
+    
+    private final MouseWheelListener mouseWheelListener;
     
 	/**
      * Creates new form NeighborhoodPanel
@@ -55,27 +77,145 @@ public class NeighborhoodPanel extends javax.swing.JPanel {
 	 * @param hasDepColor 
 	 * @param depOnColor 
      */
-    public NeighborhoodPanel(DataModel dataModel, Table table, GraphicalDataModelView graphView, boolean hideIgnored, Color depOnColor, Color hasDepColor, Color assocWithColor, Color ignoredColor) {
-        initComponents();
+    public NeighborhoodPanel(final DataModel dataModel, final Table table, final GraphicalDataModelView graphView, final boolean hideIgnored, final Color depOnColor, final Color hasDepColor, final Color assocWithColor, final Color ignoredColor) {
+        this.depOnColor = depOnColor;
+        this.hasDepColor = hasDepColor;
+        this.assocWithColor = assocWithColor;
+        this.ignoredColor = ignoredColor;
+    	initComponents();
         
-        tableLabel.setText(dataModel.getDisplayName(table));
+    	mouseWheelListener = new MouseWheelListener() {
+    		@Override
+    		public void mouseWheelMoved(MouseWheelEvent e) {
+				int newOffset = Math.max(Math.min(offset + e.getWheelRotation(), sortedTableList.size() - length), 0);
+				if (offset != newOffset) {
+					offset = newOffset;
+					createTableLinks(dataModel, table, graphView, hideIgnored);
+				}
+    		}
+    	};
+    	jPanel1.addMouseWheelListener(mouseWheelListener);
+    	
+        tableLabel.setText(dataModel.getDisplayName(table) + "  ");
         titlePanel.setBackground(WHITE);
-        
-        Font font = new JLabel("normal").getFont();
+        jPanel2.setBackground(WHITE);
+        jPanel3.setBackground(WHITE);
+
+        JLabel jLabel = new JLabel("normal");
+		Font font = jLabel.getFont();
         Font boldFont = new Font(font.getName(), font.getStyle() | Font.BOLD, font.getSize());
         italicFont = new Font(font.getName(), font.getStyle() | Font.ITALIC, font.getSize());
 		tableLabel.setFont(boldFont);
-		
-		try {
-			createTableLinks(dataModel, table, graphView, hideIgnored, depOnColor, hasDepColor, assocWithColor, ignoredColor);
-		} catch (Exception e) {
-			e.printStackTrace();
+
+		length = graphView.getHeight() / (getFontMetrics(font).getHeight() + 2) - 12;
+		if (length < 2) {
+			length = 2;
 		}
+		
+		initTableList(dataModel, table, hideIgnored);
+		createTableLinks(dataModel, table, graphView, hideIgnored);
+		
+		upLabel.addMouseListener(new MouseEventHandler(jPanel2) {
+			@Override
+			protected void onRightClick(MouseEvent e) {
+			}
+			@Override
+			protected void onLeftClick(MouseEvent e) {
+				if (offset > 0) {
+					offset = Math.max(offset - length + 1, 0);
+					createTableLinks(dataModel, table, graphView, hideIgnored);
+				}
+			}
+		});
+		downLabel.addMouseListener(new MouseEventHandler(jPanel3) {
+			@Override
+			protected void onRightClick(MouseEvent e) {
+			}
+			@Override
+			protected void onLeftClick(MouseEvent e) {
+				if (offset < sortedTableList.size() - length) {
+					offset = Math.min(offset + length - 1, sortedTableList.size() - length);
+					createTableLinks(dataModel, table, graphView, hideIgnored);
+				}
+			}
+		});
     }
 
-    private void createTableLinks(final DataModel dataModel, Table table, GraphicalDataModelView graphView, boolean hideIgnored, Color depOnColor, Color hasDepColor, Color assocWithColor, Color ignoredColor) {
-    	Set<Table> destinations = new HashSet<Table>();
-    	Map<Table, Color> tabColor = new HashMap<Table, Color>();
+	private Map<Table, Color> tabColor;
+	private List<Table> sortedTableList;
+
+	private void createTableLinks(final DataModel dataModel, final Table table, final GraphicalDataModelView graphView, final boolean hideIgnored) {
+    	if (sortedTableList.isEmpty()) {
+    		setVisible(false);
+    	} else {
+    		jPanel1.removeAll();
+    		initUpAndDownButtons(sortedTableList);
+    		int y = 0;
+    		for (final Table t: sortedTableList) {
+    			++y;
+    			if (y < offset + 1 || y > length + offset) {
+    				continue;
+    			}
+    			JLabel jLabel = new JLabel("  " + dataModel.getDisplayName(t) + "  ");
+    			JPanel jPanel = new JPanel();
+    			jLabel.addMouseWheelListener(mouseWheelListener);
+    			jLabel.addMouseListener(new MouseEventHandler(graphView.isTableVisible(t)? null : jPanel) {
+					@Override
+					protected void onRightClick(MouseEvent e) {
+						if (t != null) {
+							JPopupMenu popup = graphView.createPopupMenu(t, true);
+							popup.show(e.getComponent(), e.getX(), e.getY());
+							popup.addPopupMenuListener(new PopupMenuListener() {
+								@Override
+								public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+								}
+								@Override
+								public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+									SwingUtilities.invokeLater(new Runnable() {
+										@Override
+										public void run() {
+											initTableList(dataModel, table, hideIgnored);
+											createTableLinks(dataModel, table, graphView, hideIgnored);
+										}
+									});
+								}
+								@Override
+								public void popupMenuCanceled(PopupMenuEvent e) {
+								}
+							});
+						}
+					}
+					@Override
+					protected void onLeftClick(MouseEvent e) {
+						if (!graphView.isTableVisible(t)) {
+							graphView.showTable(table, t);
+							initTableList(dataModel, table, hideIgnored);
+							createTableLinks(dataModel, table, graphView, hideIgnored);
+						}
+					}
+				});
+    			jPanel.setLayout(new FlowLayout(0, 0, 0));
+    			jPanel.add(jLabel);
+    			jPanel.setBackground(WHITE);
+    			jLabel.setForeground(tabColor.get(t));
+    			if (graphView.isTableVisible(t)) {
+    				jLabel.setFont(italicFont);
+    				jLabel.setForeground(new Color(jLabel.getForeground().getRed(), jLabel.getForeground().getGreen(), jLabel.getForeground().getBlue(), 150));
+    			}
+    	        GridBagConstraints gridBagConstraints = new java.awt.GridBagConstraints();
+    	        gridBagConstraints.gridx = 1;
+    	        gridBagConstraints.gridy = y;
+    	        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+    	        gridBagConstraints.insets = new java.awt.Insets(2, 0, 0, 0);
+    	        jPanel1.add(jPanel, gridBagConstraints);
+    		}
+    		jPanel1.revalidate();
+    	}
+	}
+
+	private void initTableList(final DataModel dataModel, Table table, boolean hideIgnored) {
+		Set<Table> destinations = new HashSet<Table>();
+    	tabColor = new HashMap<Table, Color>();
     	
     	for (Association a: table.associations) {
     		if (!a.destination.equals(table)) {
@@ -95,36 +235,18 @@ public class NeighborhoodPanel extends javax.swing.JPanel {
     			}
     		}
     	}
-    	if (destinations.isEmpty()) {
-    		setVisible(false);
-    	} else {
-    		List<Table> sorted = new ArrayList<Table>(destinations);
-    		Collections.sort(sorted, new Comparator<Table>() {
-				@Override
-				public int compare(Table o1, Table o2) {
-					return dataModel.getDisplayName(o1).compareTo(dataModel.getDisplayName(o2));
-				}
-			});
-    		int y = 0;
-    		for (Table t: sorted) {
-    			JLabel jLabel = new JLabel("  " + dataModel.getDisplayName(t));
-    			JPanel jPanel = new JPanel();
-    			jPanel.setLayout(new FlowLayout(0, 0, 0));
-    			jPanel.add(jLabel);
-    			jPanel.setBackground(WHITE);
-    			jLabel.setForeground(tabColor.get(t));
-    			if (graphView.isTableVisible(t)) {
-    				jLabel.setFont(italicFont);
-    				jLabel.setForeground(new Color(jLabel.getForeground().getRed(), jLabel.getForeground().getGreen(), jLabel.getForeground().getBlue(), 150));
-    			}
-    	        GridBagConstraints gridBagConstraints = new java.awt.GridBagConstraints();
-    	        gridBagConstraints.gridx = 1;
-    	        gridBagConstraints.gridy = y++;
-    	        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
-    	        gridBagConstraints.insets = new java.awt.Insets(2, 0, 0, 0);
-    	        jPanel1.add(jPanel, gridBagConstraints);
-    		}
-    	}
+    	sortedTableList = new ArrayList<Table>(destinations);
+		Collections.sort(sortedTableList, new Comparator<Table>() {
+			@Override
+			public int compare(Table o1, Table o2) {
+				return dataModel.getDisplayName(o1).compareTo(dataModel.getDisplayName(o2));
+			}
+		});
+	}
+
+	private void initUpAndDownButtons(List<Table> sorted) {
+		upLabel.setVisible(offset > 0);
+		downLabel.setVisible(sorted.size() > length + offset);
 	}
 
 	/**
@@ -141,14 +263,21 @@ public class NeighborhoodPanel extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         tableLabel = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        downLabel = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        upLabel = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
 
         setOpaque(false);
         setLayout(new java.awt.GridBagLayout());
 
         titlePanel.setLayout(new java.awt.GridBagLayout());
 
-        jLabel1.setText("Neighborhood of");
+        jLabel1.setText("Neighborhood of  ");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
@@ -175,20 +304,122 @@ public class NeighborhoodPanel extends javax.swing.JPanel {
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 8);
         add(titlePanel, gridBagConstraints);
 
+        jPanel3.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, 0, 0));
+
+        jLabel3.setText(" ");
+        jPanel3.add(jLabel3);
+
+        downLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/net/sf/jailer/ui/resource/dropdown.png"))); // NOI18N
+        downLabel.setText("more  ");
+        jPanel3.add(downLabel);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 8);
+        add(jPanel3, gridBagConstraints);
+
         jPanel1.setOpaque(false);
         jPanel1.setLayout(new java.awt.GridBagLayout());
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 2, 8);
+        add(jPanel1, gridBagConstraints);
+
+        jPanel2.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, 0, 0));
+
+        jLabel2.setText(" ");
+        jPanel2.add(jLabel2);
+
+        upLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/net/sf/jailer/ui/resource/upbutton.png"))); // NOI18N
+        upLabel.setText("more  ");
+        jPanel2.add(upLabel);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
-        gridBagConstraints.insets = new java.awt.Insets(0, 0, 8, 8);
-        add(jPanel1, gridBagConstraints);
+        gridBagConstraints.insets = new java.awt.Insets(2, 0, 0, 8);
+        add(jPanel2, gridBagConstraints);
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 5;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 2, 0);
+        add(jPanel4, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel downLabel;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel tableLabel;
     private javax.swing.JPanel titlePanel;
+    private javax.swing.JLabel upLabel;
     // End of variables declaration//GEN-END:variables
+    
+    private abstract class MouseEventHandler implements MouseListener {
+
+    	private final JPanel background;
+    	
+    	public MouseEventHandler(JPanel background) {
+    		this.background = background;
+    	}
+    	
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getButton() == MouseEvent.BUTTON1) {
+				onLeftClick(e);
+			} else if (e.getButton() == MouseEvent.BUTTON3) {
+				onRightClick(e);
+			}
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			if (background != null) {
+				background.setBackground(SELECTED_COLOR);
+			}
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			if (background != null) {
+				background.setBackground(WHITE);
+			}
+		}
+    	
+		protected abstract void onLeftClick(MouseEvent e);
+		
+		protected abstract void onRightClick(MouseEvent e);
+		
+    }
+    
 }
