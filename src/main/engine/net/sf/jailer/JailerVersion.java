8b5cb46be5720729cd112790039271d8695e809f
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2017-08-22 07:27:24 +0000

    Bugfix: happened when using import-filters
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1350 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "7.3.1";
+	public static final String VERSION = "7.3.1.p";
 	
 	/**
 	 * The Jailer working tables version.
