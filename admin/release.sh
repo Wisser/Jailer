0bb779310c7dadb3f41754db7c7609f21e65663a
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2018-02-17 18:36:14 +0000

    version variable
    git-svn-id: https://svn.code.sf.net/p/jailer/code/trunk@1568 3dd849cd-670e-4645-a7cd-dd197c8d0e81

--- admin/release.sh
+++ admin/release.sh
@@ -5,7 +5,7 @@ mkdir ~/tmp/jailer
 mkdir ~/tmp/$1
 mkdir ~/tmp/$1.co
 cd ~/tmp/$1.co
-svn checkout --username=rwisser https://svn.code.sf.net/p/jailer/code/trunk
+svn checkout --username=rwisser http://svn.code.sf.net/p/jailer/code/trunk
 cd ..
 mv $1.co/trunk/* jailer
 cd jailer
@@ -13,11 +13,34 @@ find -iname ".svn" -exec rm -rf '{}' \;
 
 ant package
 
+rm -rf docs
+rm -rf out
+
+rm -rf ~/.wine/drive_c/tmp/jailer
+cp -r . ~/.wine/drive_c/tmp/jailer/
+rm -rf ~/.wine/drive_c/tmp/jailer/admin
+
+sed s/%VERSION%/$1/g admin/Jailer.nsi > admin/tmp.nsi
+wine ~/nsis-3.03/makensis.exe admin/tmp.nsi
+rm admin/tmp.nsi
+
+rm -rf ~/.wine/drive_c/tmp/dbeauty
+cp -r . ~/.wine/drive_c/tmp/dbeauty/
+rm -rf ~/.wine/drive_c/tmp/dbeauty/admin
+
+sed s/%VERSION%/$1/g admin/dbeauty.nsi > admin/tmp.nsi
+wine ~/nsis-3.03/makensis.exe admin/tmp.nsi
+rm admin/tmp.nsi
+
+sed s/%VERSION%/$1/g admin/Jailer.nsi > admin/tmp.nsi
+wine ~/nsis-3.03/makensis.exe admin/tmp.nsi
+rm admin/tmp.nsi
+
+cp admin/*nstall* ..
+
 chmod a+x *.sh
 
 rm -rf admin
-rm -rf docs
-rm -rf out
 
 cd ..
 rm $1.zip
