609e4c823c7dcf2de5eed7b565e1028af47b7fc2
Wisser <RalfWisser@gmx.net>
2018-04-28 12:37:09 +0200

    updated comment

--- src/main/engine/net/sf/jailer/Jailer.java
+++ src/main/engine/net/sf/jailer/Jailer.java
@@ -81,8 +81,7 @@ public class Jailer {
 	/**
 	 * Main-method for CLI.
 	 * 
-	 * @param args
-	 *            arguments
+	 * @param args arguments
 	 */
 	public static void main(String[] args) {
 		final Thread mainThread = Thread.currentThread();
