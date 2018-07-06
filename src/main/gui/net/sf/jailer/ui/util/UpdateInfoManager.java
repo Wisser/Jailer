04a5df43e84c55bbf19677c8d272f56e238d2571
Wisser <RalfWisser@gmx.net>
2018-07-06 10:18:08 +0200

    don't set java.net.useSystemProxies

--- src/main/gui/net/sf/jailer/ui/util/UpdateInfoManager.java
+++ src/main/gui/net/sf/jailer/ui/util/UpdateInfoManager.java
@@ -38,7 +38,6 @@ public class UpdateInfoManager {
 		Runnable check = new Runnable() {
 			@Override
 			public void run() {
-				String useSystemProxies = System.getProperty("java.net.useSystemProxies");
 				try {
 					File lastTSFile = Environment.newFile(LAST_TS_FILE);
 					if (lastTSFile.exists()) {
@@ -50,7 +49,6 @@ public class UpdateInfoManager {
 						}
 					}
 					
-					System.setProperty("java.net.useSystemProxies", "true");
 					URI uri = new URI(versionURL);
 					URLConnection connection = uri.toURL().openConnection();
 			        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -78,12 +76,6 @@ public class UpdateInfoManager {
 			        }
 				} catch (Throwable t) {
 					// ignore
-				} finally {
-					if (useSystemProxies == null) {
-						System.clearProperty("java.net.useSystemProxies");
-					} else {
-						System.setProperty("java.net.useSystemProxies", useSystemProxies);
-					}
 				}
 			}
 		};
