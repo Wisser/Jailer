8a0ef29cda322efcf18d2771493013040e3b130b
Wisser <RalfWisser@gmx.net>
2018-08-30 16:25:42 +0200

    set version to 7.11

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "7.10.9";
+	public static final String VERSION = "7.11";
 
 	/**
 	 * The Jailer working tables version.
