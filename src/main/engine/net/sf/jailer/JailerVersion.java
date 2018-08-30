029459a9d59e11043311575b12935700e95b8d90
Wisser <RalfWisser@gmx.net>
2018-08-30 16:17:02 +0200

    set version

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "7.11";
+	public static final String VERSION = "7.10.9";
 
 	/**
 	 * The Jailer working tables version.
