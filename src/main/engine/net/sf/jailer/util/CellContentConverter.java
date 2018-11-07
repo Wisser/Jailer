7edf26d7f0e708d63e18ad914c1e02226061d60b
Wisser <RalfWisser@gmx.net>
2018-11-07 12:40:57 +0100

    added equals and hasCode

--- src/main/engine/net/sf/jailer/util/CellContentConverter.java
+++ src/main/engine/net/sf/jailer/util/CellContentConverter.java
@@ -218,12 +218,64 @@ public class CellContentConverter {
 		public String toString() {
 			return value;
 		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((type == null) ? 0 : type.hashCode());
+			result = prime * result + ((value == null) ? 0 : value.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			PObjectWrapper other = (PObjectWrapper) obj;
+			if (type == null) {
+				if (other.type != null)
+					return false;
+			} else if (!type.equals(other.type))
+				return false;
+			if (value == null) {
+				if (other.value != null)
+					return false;
+			} else if (!value.equals(other.value))
+				return false;
+			return true;
+		}
 	}
 
 	public static class NCharWrapper {
 		private final String value;
 		public NCharWrapper(String value) {
 			this.value = value;
+		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((value == null) ? 0 : value.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			NCharWrapper other = (NCharWrapper) obj;
+			if (value == null) {
+				if (other.value != null)
+					return false;
+			} else if (!value.equals(other.value))
+				return false;
+			return true;
 		}
 		@Override
 		public String toString() {
