837921387e095c0e21081f551cdc92fba3a5b1f9
Wisser <RalfWisser@gmx.net>
2018-11-08 11:38:54 +0100

    Warn if the UPK is too big to be indexed.

--- src/main/engine/net/sf/jailer/ddl/DDLCreator.java
+++ src/main/engine/net/sf/jailer/ddl/DDLCreator.java
@@ -102,9 +102,11 @@ public class DDLCreator {
 	 * Creates the DDL for the working-tables.
 	 */
 	public boolean createDDL(DataModel datamodel, Session session, WorkingTableScope temporaryTableScope, RowIdSupport rowIdSupport, String workingTableSchema) throws FileNotFoundException, IOException, SQLException {
+		uPKWasTooLong = false;
 		try {
 			return createDDL(datamodel, session, temporaryTableScope, 0, rowIdSupport, workingTableSchema);
 		} catch (SQLException e) {
+			uPKWasTooLong = true;
 		}
 		// reconnect and retry with another index type
 		session.reconnect();
@@ -116,7 +118,9 @@ public class DDLCreator {
 		session.reconnect();
 		return createDDL(datamodel, session, temporaryTableScope, 2, rowIdSupport, workingTableSchema);
 	}
-		
+
+	public static boolean uPKWasTooLong = false;
+
 	/**
 	 * Creates the DDL for the working-tables.
 	 */
