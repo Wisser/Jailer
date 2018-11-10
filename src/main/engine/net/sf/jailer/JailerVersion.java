cdcc00042d71f40a41c42fe47b169be92e42ab74
Wisser <RalfWisser@gmx.net>
2018-11-10 07:39:18 +0100

    WORKING_TABLE_VERSION = 4

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -30,7 +30,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer working tables version.
 	 */
-	public static final int WORKING_TABLE_VERSION = 3;
+	public static final int WORKING_TABLE_VERSION = 4;
 
 	/**
 	 * The Jailer application name.
