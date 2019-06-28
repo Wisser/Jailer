30e07c5796b5b9e20d441c1abb86edfdfb2cdfeb
ralfw <ralfw@192.168.0.173>
2019-06-28 17:10:32 +0200

    8.7.7.0

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "8.7.7";
+	public static final String VERSION = "8.7.7.0";
 
 	/**
 	 * The Jailer working tables version.
