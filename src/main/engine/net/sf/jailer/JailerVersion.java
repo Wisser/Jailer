f637833e0910e09bde91c06e0c38b71953aed05f
Wisser <RalfWisser@gmx.net>
2019-10-15 15:47:23 +0200

    9.1.1

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "9.1.0.10";
+	public static final String VERSION = "9.1.1";
 
 	/**
 	 * The Jailer working tables version.
