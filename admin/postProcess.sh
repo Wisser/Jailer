92d5e3b99db0ab12f40748d62aa40885e6570bff
Wisser <RalfWisser@gmx.net>
2019-11-07 17:13:05 +0100

    new

---
+++ admin/postProcess.sh
@@ -0,0 +1,20 @@
+dir=`pwd`
+echo $dir
+rm -rf result
+mkdir result
+for filename in *.zip; do 
+	echo $filename
+	rm -rf ~/_
+	mkdir ~/_
+	unzip $filename -d ~/_ > /dev/null
+	chmod a+x ~/_/jailer/*.sh
+	chmod a+x ~/_/dbeauty/*.sh
+	chmod a+x ~/_/jailer/*.bat
+	chmod a+x ~/_/dbeauty/*.bat
+	chmod a+x ~/_/jailer/*.exe
+	chmod a+x ~/_/dbeauty/*.exe
+	cd ~/_
+	zip -r $dir/result/$filename *
+	cd $dir
+	pwd
+done
