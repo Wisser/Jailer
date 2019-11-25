f8cedd554ad6356c1461533d31ae00c87b1ee1d7
Wisser <RalfWisser@gmx.net>
2019-11-25 10:47:45 +0100

    Revert "9.1.8"
    This reverts commit 34076bed572f80452925fe7dfc667cee31f90430.

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "9.1.8";
+	public static final String VERSION = "9.1.7.6";
 
 	/**
 	 * The Jailer working tables version.
