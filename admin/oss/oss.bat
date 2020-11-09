5fc3ee0a01645ee11968cfb127c77e03f82395bc
Wisser <RalfWisser@gmx.net>
2020-11-09 10:34:34 +0100

    new pom template

--- admin/oss/oss.bat
+++ admin/oss/oss.bat
@@ -1,5 +1,5 @@
-gpg -ab jailer-engine-X.jar
-gpg -ab jailer-engine-X-sources.jar
-gpg -ab jailer-engine-X-javadoc.jar
-gpg -ab jailer-engine-X.pom
-jar -cvf bundle.jar *.jar *.pom *.asc
+gpg -ab jailer-engine-VERSION.jar
+gpg -ab jailer-engine-VERSION-sources.jar
+gpg -ab jailer-engine-VERSION-javadoc.jar
+gpg -ab jailer-engine-VERSION.pom
+zip bundle.jar *.jar *.pom *.asc
