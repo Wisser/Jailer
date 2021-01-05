ffb41296949a3031c9ef09248bd3684cdf42823f
Wisser <RalfWisser@gmx.net>
2021-01-05 11:40:49 +0100

    10.2.7.1

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "10.2.7";
+	public static final String VERSION = "10.2.7.1";
 
 	/**
 	 * The Jailer working tables version.
