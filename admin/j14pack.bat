335e8f8c45f661f2243ea49c700873c42b9f4a44
Wisser <RalfWisser@gmx.net>
2021-11-04 11:01:46 +0100

    changed artifact name

--- admin/j14pack.bat
+++ admin/j14pack.bat
@@ -73,5 +73,5 @@ echo "" > .singleuser
 jlink --add-modules java.se --output jre%version% 
 jpackage --name "Jailer" --input . --main-jar jailer.jar --type msi --icon jailer.ico --win-menu --win-menu-group Jailer --vendor Wisser --app-version %version% --win-upgrade-uuid d636b4ee-6f10-451e-bf57-c89656780e22 --add-launcher "Jailer Data Browser"=databrowserlauncher.properties --runtime-image jre%version%
 
-move *.msi C:\Users\ralfw\tmp\"Jailer-%version%-with-java-JRE.msi"
+move *.msi C:\Users\ralfw\tmp\"Jailer-%version%.msi"
 move *.msi c:\tmp\"Jailer-%version%.msi"
