0ef8a6a3c4bf5f21e719c769a2097ef6f90e83b2
Ralf Wisser <ralf.wisser@collenda.com>
2022-10-28 08:18:53 +0200

    12.8

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "12.7.4.6";
+	public static final String VERSION = "12.8";
 
 	/**
 	 * The Jailer working tables version.
