b732d7e918713bd3c42b5b87d1772331e8f1d1ed
Wisser <RalfWisser@gmx.net>
2023-05-05 09:41:42 +0200

    14.6.2

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "14.6.1.2";
+	public static final String VERSION = "14.6.2";
 
 	/**
 	 * The Jailer working tables version.
