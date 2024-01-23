152e704b02f32842ab206636a75a9ff741708161
Ralf Wisser <Ralf.Wisser@collenda.com>
2024-01-23 15:36:39 +0100

    15.5.15

--- admin/j14pack.bat
+++ admin/j14pack.bat
@@ -42,7 +42,7 @@ xcopy /S /E c:\tmp\_\lib c:\tmp\_\jdbc_lib\
 del /Q c:\tmp\_\lib\*
 del /Q c:\tmp\_\*.sh
 
-move c:\tmp\_\jdbc_lib\args4j.jar c:\tmp\_\lib\args4j.jar
+rem move c:\tmp\_\jdbc_lib\args4j.jar c:\tmp\_\lib\args4j.jar
 move c:\tmp\_\jdbc_lib\activation-1.1.1.jar c:\tmp\_\lib\activation-1.1.1.jar
 move c:\tmp\_\jdbc_lib\FastInfoset-1.2.13.jar c:\tmp\_\lib\FastInfoset-1.2.13.jar
 move c:\tmp\_\jdbc_lib\istack-commons-runtime-3.0.5.jar c:\tmp\_\lib\istack-commons-runtime-3.0.5.jar
@@ -63,6 +63,7 @@ move c:\tmp\_\jdbc_lib\tablefilter-swing-5.3.1.jar c:\tmp\_\lib\tablefilter-swin
 
 move c:\tmp\_\jdbc_lib\testflatlaf.jar c:\tmp\_\lib\testflatlaf.jar
 move c:\tmp\_\jdbc_lib\testbadj.jar c:\tmp\_\lib\testbadj.jar
+move c:\tmp\_\jdbc_lib\mariadb-java-client-2.7.3.jarr c:\tmp\_\lib\mariadb-java-client-2.7.3.jar
 
 del c:\tmp\_\jdbc_lib\dbunit-2.4.4.jar
 del c:\tmp\_\jdbc_lib\h2-1.3.160.jar
--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "15.5.14";
+	public static final String VERSION = "15.5.15";
 
 	/**
 	 * The Jailer working tables version.
