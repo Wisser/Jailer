31aeb46140f8b1c395c5932309039b348fea3447
Wisser <RalfWisser@gmx.net>
2024-04-29 18:56:15 +0200

    16.1.2-yaml

--- src/main/gui/net/sf/jailer/ui/ddl_script_generator/DDLScriptGeneratorPanel.java
+++ src/main/gui/net/sf/jailer/ui/ddl_script_generator/DDLScriptGeneratorPanel.java
@@ -11,24 +11,18 @@ import java.awt.Window;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -49,15 +43,8 @@ import javax.swing.ListCellRenderer;
 import javax.swing.WindowConstants;
 
 import org.fife.rsta.ui.EscapableDialog;
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.LoaderOptions;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.SafeConstructor;
-import org.yaml.snakeyaml.representer.Representer;
-import org.yaml.snakeyaml.resolver.Resolver;
 
 import liquibase.CatalogAndSchema;
-import liquibase.GlobalConfiguration;
 import liquibase.LabelExpression;
 import liquibase.Liquibase;
 import liquibase.change.Change;
@@ -94,7 +81,6 @@ import liquibase.exception.CommandExecutionException;
 import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
 import liquibase.exception.UnexpectedLiquibaseException;
-import liquibase.parser.core.yaml.YamlSnapshotParser;
 import liquibase.resource.FileSystemResourceAccessor;
 import liquibase.snapshot.DatabaseSnapshot;
 import liquibase.snapshot.EmptyDatabaseSnapshot;
@@ -107,7 +93,6 @@ import liquibase.structure.DatabaseObject;
 import liquibase.structure.core.Index;
 import liquibase.structure.core.PrimaryKey;
 import liquibase.structure.core.UniqueConstraint;
-import liquibase.util.SnakeYamlUtil;
 import liquibase.util.StringUtil;
 import net.sf.jailer.ExecutionContext;
 import net.sf.jailer.JailerVersion;
@@ -373,7 +358,6 @@ public abstract class DDLScriptGeneratorPanel extends javax.swing.JPanel {
 				}
 				
 				if (createRadioButton.isSelected() || createAndDropRadioButton.isSelected()) {
-					checkRemarks(changeLogFile);
 					out.println("-- Create objects:");
 					out.println();
 					
@@ -863,7 +847,7 @@ public abstract class DDLScriptGeneratorPanel extends javax.swing.JPanel {
         jPanel1.add(createRadioButton, gridBagConstraints);
 
         createAndDropRadioButton.setText("Drop and Create");
-        createAndDropRadioButton.setToolTipText("<html>Generate “<b>DROP</b> TABLE/VIEW/INDEX etc.” statements, followed by “<b>CREATE</b> ...” statements.</html>");
+        createAndDropRadioButton.setToolTipText("<html>Generate \"<b>DROP</b> TABLE/VIEW/INDEX etc.\" statements, followed by \"<b>CREATE</b> ...\" statements.</html>");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 19;
@@ -1021,106 +1005,6 @@ public abstract class DDLScriptGeneratorPanel extends javax.swing.JPanel {
 			scriptFileTextField.setText(Environment.makeRelative(fn));
 		}
     }//GEN-LAST:event_fileFindButtonActionPerformed
-
-    /**
-     * Replace any characters that will cause problems when parsing remarks in the change log file with '?'.
-     */
-	private void checkRemarks(File changeLogFile) {
-        LoaderOptions loaderOptions = new LoaderOptions();
-        SnakeYamlUtil.setCodePointLimitSafely(loaderOptions, YamlSnapshotParser.CODE_POINT_LIMIT);
-        Representer representer = new Representer(new DumperOptions());
-        DumperOptions dumperOptions = new DumperOptions();
-        dumperOptions.setDefaultFlowStyle(representer.getDefaultFlowStyle());
-        dumperOptions.setDefaultScalarStyle(representer.getDefaultScalarStyle());
-        dumperOptions
-                .setAllowReadOnlyProperties(representer.getPropertyUtils().isAllowReadOnlyProperties());
-        dumperOptions.setTimeZone(representer.getTimeZone());
-        Yaml yaml = new Yaml(new SafeConstructor(loaderOptions), representer, dumperOptions, loaderOptions, new Resolver());
-
-		try {
-			Object parsedYaml;
-			try (InputStream stream = new FileInputStream(changeLogFile)) {
-				try (InputStreamReader inputStreamReader = new InputStreamReader(stream,
-						GlobalConfiguration.OUTPUT_FILE_ENCODING.getCurrentValue())) {
-					parsedYaml = yaml.load(inputStreamReader);
-				}
-			}
-			if (checkRemarks(parsedYaml)) {
-				try (Writer outWriter = new FileWriter(changeLogFile)) {
-					yaml.dump(parsedYaml, outWriter);
-				}
-			}
-		} catch (Exception e) {
-			LogUtil.warn(e);
-		}
-	}
-
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-	private boolean checkRemarks(Object yaml) {
-    	boolean modified = false;
-    	
-    	if (yaml instanceof Map) {
-			HashSet<?> keys = new HashSet<>(((Map) yaml).keySet());
-			for (Object key: keys) {
-				Object value = ((Map) yaml).get(key);
-				if ("remarks".equals(key) && value instanceof byte[]) {
-					modified = true;
-					((Map) yaml).put(key, checkRemark(new String((byte[]) value)));
-				}
-				if (checkRemarks(value)) {
-					modified = true;
-				}
-			}
-		} else if (yaml instanceof Collection) {
-			for (Object item : ((Collection) yaml)) {
-				if (checkRemarks(item)) {
-					modified = true;
-				}
-			}
-		}
-		return modified;
-	}
-
-	/**
-     * Replace any characters that will cause problems when parsing the change log file with '?'.
-     */
-    private String checkRemark(String text) throws UnexpectedLiquibaseException {
-        if (null == text || text.isEmpty()) {
-            return text;
-        }
-
-        final int len = text.length();
-        char current;
-        int codePoint;
-        boolean modified = false;
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < len; i++) {
-            current = text.charAt(i);
-            if (Character.isHighSurrogate(current) && i + 1 < len && Character.isLowSurrogate(text.charAt(i + 1))) {
-                codePoint = text.codePointAt(i++);
-            } else {
-                codePoint = current;
-            }
-            if ((codePoint == '\n')
-                    || (codePoint == '\r')
-                    || (codePoint == '\t')
-                    || (codePoint == 0xB)
-                    || (codePoint == 0xC)
-                    || ((codePoint >= 0x20) && (codePoint <= 0x7E))
-                    || ((codePoint >= 0xA0) && (codePoint <= 0xD7FF))
-                    || ((codePoint >= 0xE000) && (codePoint <= 0xFFFD))
-                    || ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF))
-                    ) {
-                sb.append(current);
-            } else {
-                sb.append('?');
-                modified = true;
-            }
-        }
-
-        return modified? sb.toString() : text;
-    }
 
 	private String toFileName(String f) {
 		if (!new File(f).isAbsolute()) {
