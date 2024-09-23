02f6ee53be15be0320fa145ef74cd85a28ba17c4
Ralf Wisser <ralf.wisser@aryza.com>
2024-09-23 10:32:23 +0200

    jdk 23

--- admin/j14pack.bat
+++ admin/j14pack.bat
@@ -81,7 +81,5 @@ echo "" > .singleuser
 jlink --add-options="-Xmx4000m" --add-modules java.se --output jre%version% 
 jpackage --name "Jailer Database Tools" --input . --main-jar jailer.jar --type msi --icon jailer.ico --win-menu --win-menu-group Jailer --vendor Wisser --app-version %version% --win-upgrade-uuid d636b4ee-6f10-451e-bf57-c89656780e22 --runtime-image jre%version%
 
-pause
-
 move *.msi C:\Users\ralfw\tmp\"Jailer-database-tools-%version%.msi"
 move *.msi c:\tmp\"Jailer-database-tools-%version%.msi"
