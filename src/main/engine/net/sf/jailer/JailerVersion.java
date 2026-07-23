de2f2b9461c237ac08c59ddaabd5a562e0caf170
Ralf Wisser <Ralf.Wisser@aryza.com>
2026-07-23 23:08:04 +0200

    17.3

--- src/main/engine/net/sf/jailer/JailerVersion.java
+++ src/main/engine/net/sf/jailer/JailerVersion.java
@@ -25,7 +25,7 @@ public class JailerVersion {
 	/**
 	 * The Jailer version.
 	 */
-	public static final String VERSION = "17.2.0.1";
+	public static final String VERSION = "17.3";
 
 	/**
 	 * The Jailer working tables version.
